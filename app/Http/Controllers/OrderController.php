ac26c44a8a2f8fa9b8940575f87598ed0db05bc1
Dhani <mrmdhni01@gmail.com>
2020-09-26 00:19:12 +0700

    admin detail pesanan customer

---
+++ app/Http/Controllers/OrderController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Order;
+
+class OrderController extends Controller
+{
+    public function index()
+    {
+        $orders = Order::with(['customer.district.city.province'])
+                        ->orderBy('created_at', 'DESC');
+
+        if ( request()->q != '' ) {
+            $orders = $orders->where(function($q){
+                $r = request()->q;
+                $q->where('customer_name', 'LIKE','%'.$r.'%')
+                ->orWhere('invoice', 'LIKE','%'.$r.'%')
+                ->orWhere('customer_address', 'LIKE','%'.$r.'%');
+            });
+        }
+
+        if ( request()->status != '' ) {
+            $orders = $orders->where('status', request()->status);
+        }
+
+        $orders = $orders->paginate(10);
+
+        return view('orders.index', compact('orders'));
+    }
+
+    public function view($invoice)
+    {
+        $order = Order::with(['customer.district.city.province', 'payment', 'details.product'])
+                    ->where('invoice', $invoice)->first();
+        
+        return view('orders.view', compact('order'));
+    }
+
+    public function acceptPayment($invoice)
+    {
+        $order = Order::with(['payment'])->where('invoice', $invoice)->first();
+        $order->payment()->update(['status'=>1]);
+        $order->update(['status'=>2]);
+
+        return redirect(route('orders.view', $order->invoice));
+    }
+
+    public function destroy(Order $order)
+    {
+        $order->details()->delete();
+        $order->payment()->delete();
+        $order->delete();
+
+        return redirect(route('orders.index'));
+    }
+}
